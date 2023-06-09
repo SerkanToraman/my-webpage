cacde085dbc2a73f943f78d1b2c639fcb84b6683
serkantoraman <serkan@github.com>
2023-06-09 15:39:04 +0300

    map function added in projects component

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,115 +1,72 @@
 import React from 'react'
 import { useSelector } from 'react-redux';
+import pizzaImg from "../../src/img/pizzaorder.jpeg";
+import friendImg from "../../src/img/friendsLogin.jpeg";
+import movieListPic from "../../src/img/movieList.jpeg"
+
 
 const Projects = () => {
   const UIText = useSelector((store)=> store.languageReducer);
+  const projectsData = [{
+    img :pizzaImg,
+    alt:"pizzaOrder",
+    name:UIText.projectName1,
+    definition:UIText.projectDescription1,
+    skill1:"react",
+    skill2:"yup",
+    skill3:"reactstrap",
+    githubLink:"https://github.com/SerkanToraman/fsweb-s7-challenge-pizza.git",
+    websitelink:"https://serkan-toraman-pizzaorder-f9ud94m6x-serkantoraman.vercel.app/",
+    website:UIText.viewSite
+  },
+  {
+    img :friendImg,
+    alt:"friendsLogin",
+    name:UIText.projectName2,
+    definition:UIText.projectDescription2,
+    skill1:"useForm",
+    skill2:"context API",
+    skill3:"tailwind",
+    githubLink:"https://github.com/SerkanToraman/fsweb-s11g2-client-auth-friends",
+    websitelink:"https://serkantoraman-loginfriends.vercel.app/",
+    website:UIText.viewSite
+  },
+  {
+    img:movieListPic,
+    alt:"movieList",
+    name:UIText.projectName3,
+    definition:UIText.projectDescription3,
+    skill1:"react",
+    skill2:"redux",
+    skill3:"middleware",
+    githubLink:"https://github.com/SerkanToraman/fsweb-s10g3-redux-watchlist",
+    websitelink:"https://serkan-toraman-movielist.vercel.app/",
+    website:UIText.viewSite
+  }
+  ]
   return (
   <section id="projects">
     <p id='projectMainHeaders'>{UIText.projects}</p>
     <div id="projectSet" >
+      {projectsData.map((item)=>
       <div id='projectBoxSet'>
         <div id='projectImg'>
-          <img id="projectImg" src={require("../../src/img/pizzaorder.jpeg")} alt="pizzaOrder"/>
+          <img id="projectImg" src={item.img} alt={item.alt}/>
         </div>
-        <p id='projectSubHeader'>{UIText.projectName1}e</p>
-        <p id='projectDefinition'>{UIText.projectDescription1}
+        <p id='projectSubHeader'>{item.name}</p>
+        <p id='projectDefinition'>{item.definition}
         </p>
         <div id='skillsUsed'>
-          <p>react</p>
-          <p>yup</p>
-          <p>reactstrap</p>
+          <p>{item.skill1}</p>
+          <p>{item.skill2}</p>
+          <p>{item.skill3}</p>
         </div>
         <div id='links'>
-          <a id="projectsLink"href="https://github.com/SerkanToraman/fsweb-s7-challenge-pizza.git">Github</a>
-          <a id="projectsLink"href="https://serkan-toraman-pizzaorder-f9ud94m6x-serkantoraman.vercel.app/">{UIText.viewSite}</a>
+          <a id="projectsLink"href={item.githubLink}>Github</a>
+          <a id="projectsLink"href={item.websitelink}>{item.website}</a>
         </div>
       </div>
-      <div id='projectBoxSet'>
-        <div id='projectImg'>
-          <img id="projectImg" src={require("../../src/img/friendsLogin.jpeg")} alt="friendsLogin"/>
-        </div>
-        <p id='projectSubHeader'>{UIText.projectName2}</p>
-        <p id='projectDefinition'>{UIText.projectDescription2}
-        </p>
-        <div id='skillsUsed'>
-          <p>useForm</p>
-          <p>context API</p>
-          <p>tailwind</p>
-        </div>
-        <div id='links'>
-          <a id="projectsLink"href="https://github.com/SerkanToraman/fsweb-s11g2-client-auth-friends">Github</a>
-          <a id="projectsLink"href="https://serkantoraman-loginfriends.vercel.app/">{UIText.viewSite}</a>
-        </div>
-      </div>
-      <div id='projectBoxSet'>
-        <div id='projectImg'>
-          <img id="projectImg" src={require("../../src/img/movieList.jpeg")} alt="movieList"/>
-        </div>
-        <p id='projectSubHeader'>{UIText.projectName3}</p>
-        <p id='projectDefinition'>{UIText.projectDescription3}
-        </p>
-        <div id='skillsUsed'>
-          <p>react</p>
-          <p>redux</p>
-          <p>middleware</p>
-        </div>
-        <div id='links'>
-          <a id="projectsLink"href="https://github.com/SerkanToraman/fsweb-s10g3-redux-watchlist">Github</a>
-          <a id="projectsLink"href="https://serkan-toraman-movielist.vercel.app/">{UIText.viewSite}</a>
-        </div>
-      </div>
-      {/* <div id='projectBoxSet'>
-        <div id='projectImg'>
-          <img id="projectImg" src="https://picsum.photos/seed/picsum/300/180" alt="projectImg"/>
-        </div>
-        <p id='projectSubHeader'>Project Name</p>
-        <p id='projectDefinition'>A simple, customizable, minimal setup cookie plugin that allows your users to select which cookies to accept or decline. This was created with vanilla JS, SCSS and Parcel Bundler and is available as a NPM package and the git repository makes any type of customization to code and themes
-        </p>
-        <div id='skillsUsed'>
-          <p>react</p>
-          <p>redux</p>
-          <p>axios</p>
-        </div>
-        <div id='links'>
-          <a id="projectsLink"href="https://www.w3schools.com">Github</a>
-          <a id="projectsLink"href="https://www.w3schools.com">View Site</a>
-        </div>
-      </div>
-      <div id='projectBoxSet'>
-        <div id='projectImg'>
-          <img id="projectImg" src="https://picsum.photos/seed/picsum/300/180" alt="projectImg"/>
-        </div>
-        <p id='projectSubHeader'>Project Name</p>
-        <p id='projectDefinition'>A simple, customizable, minimal setup cookie plugin that allows your users to select which cookies to accept or decline. This was created with vanilla JS, SCSS and Parcel Bundler and is available as a NPM package and the git repository makes any type of customization to code and themes
-        </p>
-        <div id='skillsUsed'>
-          <p>react</p>
-          <p>redux</p>
-          <p>axios</p>
-        </div>
-        <div id='links'>
-          <a id="projectsLink"href="https://www.w3schools.com">Github</a>
-          <a id="projectsLink"href="https://www.w3schools.com">View Site</a>
-        </div>
-      </div>
-      <div id='projectBoxSet'>
-        <div id='projectImg'>
-          <img id="projectImg" src="https://picsum.photos/seed/picsum/300/180" alt="projectImg"/>
-        </div>
-        <p id='projectSubHeader'>Project Name</p>
-        <p id='projectDefinition'>A simple, customizable, minimal setup cookie plugin that allows your users to select which cookies to accept or decline. This was created with vanilla JS, SCSS and Parcel Bundler and is available as a NPM package and the git repository makes any type of customization to code and themes
-        </p>
-        <div id='skillsUsed'>
-          <p>react</p>
-          <p>redux</p>
-          <p>axios</p>
-        </div>
-        <div id='links'>
-          <a id="projectsLink"href="https://www.w3schools.com">Github</a>
-          <a id="projectsLink"href="https://www.w3schools.com">View Site</a>
-        </div>
-      </div> */}
-
+      )}
     </div>  
   </section> 
   )
