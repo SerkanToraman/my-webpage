4ba64163ae31d9689389172c8881cb66231ecadc
serkantoraman <serkan@github.com>
2023-06-19 11:03:43 +0300

    Linkedin,Github button window_open error for apple solved

--- src/toastify.js
+++ src/toastify.js
@@ -1,10 +1,18 @@
 import { toast } from 'react-toastify';
 
-export const redirecting = (link,text) => {
-  toast.info(text,{ position: toast.POSITION.TOP_LEFT})
+export const redirecting = (text,link) => {
+    toast.info(text,{ position: toast.POSITION.TOP_LEFT})
   setTimeout(()=>{
-    window.open(link, '_blank')
+    // eslint-disable-next-line 
+    location.href=link;
    }, 2200);
 }
+
+// export const redirecting = (link,text) => {
+//   toast.info(text,{ position: toast.POSITION.TOP_LEFT})
+//   setTimeout(()=>{
+//     window.open(link, '_blank')
+//    }, 2200);
+// }
 
 
