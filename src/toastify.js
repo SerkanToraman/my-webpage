4a336399ed2c7a90395625e1815e8a3c477a6c27
serkantoraman <serkan@github.com>
2023-06-19 14:21:43 +0300

    window.open error trial 6

--- src/toastify.js
+++ src/toastify.js
@@ -2,9 +2,11 @@ import { toast } from 'react-toastify';
 
 export const redirecting = (link,text) => {
   toast.info(text,{ position: toast.POSITION.TOP_LEFT})
+  
+  let wi = window.open('about:blank', '_blank') 
   setTimeout(()=>{
-    window.open(link)
-   }, 2200);
+    wi.location.href = link;
+   }, 500);
 }
 
 // export const redirecting = (link,text) => {
