6e98f49ba60be410300960239246270033a9012c
serkantoraman <serkan@github.com>
2023-08-02 01:05:27 +0300

    english typo corrected

--- src/languages/en.js
+++ src/languages/en.js
@@ -1,44 +1,48 @@
-
 export const enText = {
-  themeModeDark : "NIGHT MODE",
-  themeModeLight : "LIGHT MODE", 
-  lang :  "TÜRKÇE",
+  themeModeDark: "NIGHT MODE",
+  themeModeLight: "LIGHT MODE",
+  lang: "TÜRKÇE",
   lang1: "'YE GEÇ",
-  skills:"Skills",
-  projects : "Projects",
+  skills: "Skills",
+  projects: "Projects",
   hireMe: "Hire me",
-  redirectingemail:"Redirecting to E-mail",
-  mainHeader1 :"Creative Thinker",
-  mainHeader2 :"Continuous Learner",
-  shortIntroduction:"Hi, I’m Serkan. I’m a full-stack developer. If you are looking for a Developer who to craft a solid product that all the parts are taken care of and a product that will impress the end user, let’s shake hands.",
-  skillDescription1:"I have completed over 20 project in which I have made use of redux, middleware, context API's, custom hooks, localstorage, axios, react-hook-form, react-router-dom, cypress libraries",
-  skillDescription2:"I have a deep understanding in Express, knex, sqlite3, dotenv, cors, redis, jsonwebtoken, bcryptjs, jest, nodemon libraries. Additionally, I have a deep knowledge in REST protocol.",
-  skillDescription3:"I have experience in SCSS. In addition, i have made use of several libriaries such as bootstrap, reactstrap, tailwinds, toastifty.",
-  profileHeader:"Profile",
-  DOBQuestion:"Date of Birth",
-  DOBAnswern:"30.10.1985",
-  cityQuestion:"City ​​of Residence",
-  cityAnswer:"Istanbul",
+  redirectingemail: "Redirecting to E-mail",
+  mainHeader1: "Creative Thinker",
+  mainHeader2: "Continuous Learner",
+  shortIntroduction:
+    "Hi, I’m Serkan. I’m a full-stack developer. If you are looking for a Developer who to craft a solid product that all the parts are taken care of and a product that will impress the end user, let’s shake hands.",
+  skillDescription1:
+    "I have completed over 20 project in which I have made use of redux, middleware, context API's, custom hooks, localstorage, axios, react-hook-form, react-router-dom, cypress libraries",
+  skillDescription2:
+    "I have a deep understanding in Express, knex, sqlite3, dotenv, cors, redis, jsonwebtoken, bcryptjs, jest, nodemon libraries. Additionally, I have a deep knowledge in REST protocol.",
+  skillDescription3:
+    "I have experience in SCSS. In addition, i have made use of several libriaries such as bootstrap, reactstrap, tailwinds, toastifty.",
+  profileHeader: "Profile",
+  DOBQuestion: "Date of Birth",
+  DOBAnswern: "30.10.1985",
+  cityQuestion: "City ​​of Residence",
+  cityAnswer: "Istanbul",
   educationQuestion: "Degree",
   educationAnswer: "MSc Engineering Business Mgmt,Coventry Uni.",
-  roleQuestion:"Preferred Role",
-  roleAnswer:"Full Stack Developer",
-  aboutme:"About Me",
-  aboutMeDescription:"After completing my BSc degree on Mechanical Eng. in Erciyes Uni., I moved to UK to attain MSc. degree on Engineering Business Management in Coventry University. Following my Master Degree, I worked in severel different countries around the world; Italy, China, Turkey. I also have had the opportunity work in different levels. Even I have mostly worked in Industrial Engineering department, I also have experience in Sales and Marketing. I aim to succeed in my programming career considering my passion towards it together with multicultural work experience and my continues learning mindset. ",
-  projectName1:"Pizza Order",
-  projectDescription1:"A simple pizza order web page in which all the items price are shown and the end user can see the sub and final price. React is appplied in this project. Prop drilling is the tool that used to send data between components. Additionaly Yup validation is added and reactstrap is used for the styling. So what are your favourite toppings?  ",
-  projectName2:"Twitter Project",
-  projectDescription2:"This project emcompasses the both backend and frontend coding. ReactJS library for front-end and NodeJS back-end service are applied to code the project. Additionally, SCSS languguage is used for the stying. The project is live and you can send a tweet.  ",
-  projectName3:"Movie List",
-  projectDescription3:"Acions, reducers, dispatch, useSelector, middleware etc. in other words Redux is used in this project. Some simple yet nice styling is done with tailwind. You can also create a list out of your fav movies. So what are your fav movies? ",
-  viewSite:"View Site",
-  footerComment:"Let’s work together on your next product.",
-  personelBlog:"Personal Blog",
-  redirectingLinkedin:"Redirecting to Linkedin",
-  redirectingGithub:"Redirecting to Github",
-  redirectingHackerrank:"Redirecting to Hackerrank",
-  pageUnderConstruction:"Page is underconstruction"
-
-
-
-}
+  roleQuestion: "Preferred Role",
+  roleAnswer: "Full Stack Developer",
+  aboutme: "About Me",
+  aboutMeDescription:
+    "After completing my BSc degree on Mechanical Eng. in Erciyes Uni., I moved to UK to attain MSc. degree on Engineering Business Management in Coventry University. Following my Master Degree, I worked in severel different countries around the world; Italy, China, Turkey. I also have had the opportunity work in different levels. Even I have mostly worked in Industrial Engineering department, I also have experience in Sales and Marketing. I aim to succeed in my programming career considering my passion towards it together with multicultural work experience and my continues learning mindset. ",
+  projectName1: "Pizza Order",
+  projectDescription1:
+    "A simple pizza order web page in which all the items price are shown and the end user can see the sub and final price. React is appplied in this project. Prop drilling is the tool that used to send data between components. Additionaly Yup validation is added and reactstrap is used for the styling. So what are your favourite toppings?  ",
+  projectName2: "Twitter Project",
+  projectDescription2:
+    "This project encompasses the both backend and frontend coding. ReactJS library for front-end and NodeJS back-end service are applied to code the project. Additionally, SCSS language is used for the stying. The project is live and you can send a tweet.",
+  projectName3: "Movie List",
+  projectDescription3:
+    "Acions, reducers, dispatch, useSelector, middleware etc. in other words Redux is used in this project. Some simple yet nice styling is done with tailwind. You can also create a list out of your fav movies. So what are your fav movies? ",
+  viewSite: "View Site",
+  footerComment: "Let’s work together on your next product.",
+  personelBlog: "Personal Blog",
+  redirectingLinkedin: "Redirecting to Linkedin",
+  redirectingGithub: "Redirecting to Github",
+  redirectingHackerrank: "Redirecting to Hackerrank",
+  pageUnderConstruction: "Page is underconstruction",
+};
